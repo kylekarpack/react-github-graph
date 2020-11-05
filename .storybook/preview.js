3aa181decbb754a75383dcaa3f6ac92f090b1f3c
Kyle Karpack <karpack@adobe.com>
2020-11-04 17:04:07 -0800

    Use docs mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,10 @@
+import { addParameters } from "@storybook/client-api";
+
+addParameters({
+	viewMode: "docs",
+});
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	viewMode: "docs",
+};
