a7617e152fa47320c17bff4931676463e372bfc3
Kyle Karpack <karpack@adobe.com>
2020-11-04 16:14:55 -0800

    Basic storybook implementation

---
+++ src/stories/0-GithubContrbutions.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import GithubContributions from "../GithubContributions";
+
+export default {
+	title: "GithubContributions"
+};
+
+export const Story = () => (
+	<GithubContributions username="kylekarpack" />
+);
--- src/stories/0-GoodreadsBookshelf.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default {
-	title: "GitHubContributions"
-};
-
-export const Story = () => (
-	<div>test!</div>
-);
