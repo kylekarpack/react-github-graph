44c807a70f266a885b92bd8750da2d4278c4bc23
Kyle Karpack <kylekarpack@gmail.com>
2021-02-13 11:42:00 -0800

    Update to match new GitHub markup

--- src/ChartContainer.jsx
+++ src/ChartContainer.jsx
@@ -5,6 +5,67 @@ const ChartContainer = styled.div`
 		margin-bottom: 0;
 	}
 
+	.ContributionCalendar.days-selected .ContributionCalendar-day {
+		opacity: 0.5;
+	}
+
+	.ContributionCalendar.days-selected .ContributionCalendar-day.active {
+		opacity: 1;
+	}
+
+	.ContributionCalendar-label {
+		font-size: 9px;
+		fill: var(--color-text-primary);
+	}
+
+	.ContributionCalendar-day,
+	.ContributionCalendar-day[data-level="0"] {
+		fill: var(--color-calendar-graph-day-bg);
+		shape-rendering: geometricPrecision;
+		outline: 1px solid var(--color-calendar-graph-day-border);
+		outline-offset: -1px;
+	}
+
+	.ContributionCalendar-day[data-level="1"] {
+		fill: var(--color-calendar-graph-day-L1-bg);
+		outline: 1px solid var(--color-calendar-graph-day-L1-border);
+	}
+
+	.ContributionCalendar-day[data-level="2"] {
+		fill: var(--color-calendar-graph-day-L2-bg);
+		outline: 1px solid var(--color-calendar-graph-day-L2-border);
+	}
+
+	.ContributionCalendar-day[data-level="3"] {
+		fill: var(--color-calendar-graph-day-L3-bg);
+		outline: 1px solid var(--color-calendar-graph-day-L3-border);
+	}
+
+	.ContributionCalendar-day[data-level="4"] {
+		fill: var(--color-calendar-graph-day-L4-bg);
+		outline: 1px solid var(--color-calendar-graph-day-L4-border);
+	}
+
+	.ContributionCalendar[data-holiday="halloween"]
+		.ContributionCalendar-day[data-level="1"] {
+		fill: var(--color-calendar-halloween-graph-day-L1-bg);
+	}
+
+	.ContributionCalendar[data-holiday="halloween"]
+		.ContributionCalendar-day[data-level="2"] {
+		fill: var(--color-calendar-halloween-graph-day-L2-bg);
+	}
+
+	.ContributionCalendar[data-holiday="halloween"]
+		.ContributionCalendar-day[data-level="3"] {
+		fill: var(--color-calendar-halloween-graph-day-L3-bg);
+	}
+
+	.ContributionCalendar[data-holiday="halloween"]
+		.ContributionCalendar-day[data-level="4"] {
+		fill: var(--color-calendar-halloween-graph-day-L4-bg);
+	}
+
 	.contributions {
 		--color-calendar-graph-day-bg: #ebedf0;
 		--color-calendar-graph-day-border: rgba(27, 31, 35, 0.06);
