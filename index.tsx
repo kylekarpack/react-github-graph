84443982dd7856e3fe78dfda089812c8a93ac627
Kyle Karpack <kylekarpack@gmail.com>
2022-01-12 11:29:38 -0800

    Update preview pages

--- index.tsx
+++ index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import GithubContributions from "./src/GithubContributions";
+import { GithubContributions } from "./src";
+
+import { GithubContributions as Ghc } from "./dist/index.es";
 
 render(
   <div>
     <h1>React Github Graph Demo Page</h1>
 		<GithubContributions username="kylekarpack" />
+    <Ghc username="kylekarpack" />
   </div>,
   document.getElementById("home")
 );
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import GithubContributions from "./GithubContributions";
 
+export default GithubContributions;
 export { GithubContributions };
