9098401a9f26cd20faba20e246c7489562a6355e
Kyle Karpack <kylekarpack@gmail.com>
2022-12-10 09:14:35 -0800

    Storybook fixes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,18 @@
 const config = {
   stories: ["../stories/**/*.stories.{ts,tsx}"],
   addons: ["@storybook/addon-controls"],
+  features: {
+    babelModeV7: true,
+  },
   typescript: {
     check: false,
     checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
   },
   core: {
     builder: "webpack5",
